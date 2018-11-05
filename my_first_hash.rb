3de794f81d5a1dd840ec3daf48c9f225769e2672
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-05 23:50:00 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,9 +19,9 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
+retrieval['oil paintings']
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  hash['oil paintings']
+  
 end
 
 def adding
