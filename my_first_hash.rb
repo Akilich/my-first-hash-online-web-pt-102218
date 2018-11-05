7fbd762e36acd42388265f91a410c90235096b59
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-05 23:55:52 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,20 +26,17 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
+                  "whale bone corsets" => 5,
+                       "porcelain vases" => 2,
+                       "oil paintings" => 3,
+                       "muskets" => 2,
+                       "gun powder" => 4
                  }
   # add 2 muskets to the shipping_manifest hash below
-shipping_manifest = {
-                  "muskets" => 2
-                 }
 
 
   # add 4 gun powder to the shipping_manifest hash below
-shipping_manifest = {
-"gun powder" => 4
-}
+
 
   # return the shipping_manifest hash below
 hash["shipping_manifest"] = "shipping_manifest2"
